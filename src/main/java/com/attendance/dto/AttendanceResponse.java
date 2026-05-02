167829507fcd2f597d70f5c6c649ffdc774f7e8b
Abhinav Pandey <166276971+abhi017z@users.noreply.github.com>
2026-05-02 23:03:08 +0530

    update AttendanceResponse
    Removed 'id' field from AttendanceResponse class.

--- src/main/java/com/attendance/dto/AttendanceResponse.java
+++ src/main/java/com/attendance/dto/AttendanceResponse.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AttendanceResponse {
-    private Long id;
     private LocalDate date;
     private AttendanceStatus status;
     private Long studentId;
